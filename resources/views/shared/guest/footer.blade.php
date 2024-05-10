b6e1416a39822003c715b0076f36d67c57c5282a
ahmedqo <ahmedqo1995@gmail.com>
2024-05-10 19:06:46 +0000

    ggggg

--- resources/views/shared/guest/footer.blade.php
+++ resources/views/shared/guest/footer.blade.php
@@ -26,6 +26,15 @@
                                         </a>
                                     </li>
                                 @endif
+                                @if (env('LINK_FACEBOOK'))
+                                    <li>
+                                        <a href="{{ env('LINK_FACEBOOK') }}" class="flex text-x-prime text-sm">
+                                            <svg class="block w-8 h-8 pointer-events-none" fill="currentColor"
+                                                viewBox="0 0 32 32">
+                                                <path
+                                                    d="M 19.253906 2 C 15.311906 2 13 4.0821719 13 8.8261719 L 13 13 L 8 13 L 8 18 L 13 18 L 13 30 L 18 30 L 18 18 L 22 18 L 23 13 L 18 13 L 18 9.671875 C 18 7.884875 18.582766 7 20.259766 7 L 23 7 L 23 2.2050781 C 22.526 2.1410781 21.144906 2 19.253906 2 z" />
+                                            </svg>
+                                @endif
                                 @if (env('LINK_INSTAGRAM'))
                                     <li>
                                         <a href="{{ env('LINK_INSTAGRAM') }}" class="flex text-x-prime text-sm">
